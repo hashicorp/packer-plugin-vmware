8f71240b27ca49afb8491dbef098ca0d6b3b9bbf
Adrien Delorme <azr@users.noreply.github.com>
2021-08-31 16:02:39 +0200

    consolidate version location

--- main.go
+++ main.go
@@ -5,30 +5,17 @@ import (
 	"os"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
-	"github.com/hashicorp/packer-plugin-sdk/version"
+
 	"github.com/hashicorp/packer-plugin-vmware/builder/vmware/iso"
 	"github.com/hashicorp/packer-plugin-vmware/builder/vmware/vmx"
-)
-
-var (
-	// Version is the main version number that is being run at the moment.
-	Version = "1.0.1"
-
-	// VersionPrerelease is A pre-release marker for the Version. If this is ""
-	// (empty string) then it means that it is a final release. Otherwise, this
-	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
-	VersionPrerelease = "dev"
-
-	// PluginVersion is used by the plugin set to allow Packer to recognize
-	// what version this plugin is.
-	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+	"github.com/hashicorp/packer-plugin-vmware/version"
 )
 
 func main() {
 	pps := plugin.NewSet()
 	pps.RegisterBuilder("iso", new(iso.Builder))
 	pps.RegisterBuilder("vmx", new(vmx.Builder))
-	pps.SetVersion(PluginVersion)
+	pps.SetVersion(version.PluginVersion)
 	err := pps.Run()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
