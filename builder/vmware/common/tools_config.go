0c09a50732a1ddc282edba04b3b0b67526817ba6
Ryan Johnson <ryan@tenthirtyam.org>
2024-07-06 22:57:06 -0400

    refactor: tools upload flavors
    Refactors to use `allowedToolsFlavorValues` for tools upload flavor options based on constants.
    
    Signed-off-by: Ryan Johnson <ryan@tenthirtyam.org>

--- builder/vmware/common/tools_config.go
+++ builder/vmware/common/tools_config.go
@@ -7,36 +7,59 @@ package common
 
 import (
 	"fmt"
+	"slices"
+	"strings"
 
 	"github.com/hashicorp/packer-plugin-sdk/template/interpolate"
 )
 
+// Set the allowed values for the `ToolsUploadFlavor`.
+const (
+	ToolsFlavorMacOS   = "darwin"
+	ToolsFlavorLinux   = "linux"
+	ToolsFlavorWindows = "windows"
+)
+
+// allowedToolsFlavorValues is a list of allowed values for the
+// `ToolsUploadFlavor`.
+var allowedToolsFlavorValues = []string{ToolsFlavorMacOS, ToolsFlavorLinux, ToolsFlavorWindows}
+
 type ToolsConfig struct {
-	// The flavor of the VMware Tools ISO to
-	// upload into the VM. Valid values are darwin, linux, and windows. By
-	// default, this is empty, which means VMware tools won't be uploaded.
+	// The flavor of VMware tools to upload into the virtual machine based on
+	// the guest operating system. Allowed values are `darwin` (macOS), `linux`,
+	// and `windows`. Default is empty and no version will be uploaded.
 	ToolsUploadFlavor string `mapstructure:"tools_upload_flavor" required:"false"`
-	// The path in the VM to upload the VMware tools. This only takes effect if
-	// `tools_upload_flavor` is non-empty. This is a [configuration
-	// template](/packer/docs/templates/legacy_json_templates/engine) that has a single valid variable:
-	// `Flavor`, which will be the value of `tools_upload_flavor`. By default
-	// the upload path is set to `{{.Flavor}}.iso`. This setting is not used
-	// when `remote_type` is `esx5`.
+	// The path in the virtual machine to upload the VMware Tools. This only
+	// takes effect if `tools_upload_flavor` is non-empty. This is a
+	// [configuration template](/packer/docs/templates/legacy_json_templates/engine)
+	// that has a single valid variable, `Flavor`, which will be the value of
+	// `tools_upload_flavor` when the upload path is set to `{{.Flavor}}.iso`.
+	//
+	// ~> **Note:** This setting is not used when `remote_type` is `esx5`.
 	ToolsUploadPath string `mapstructure:"tools_upload_path" required:"false"`
-	// The path on your local machine to fetch the vmware tools from. If this
-	// is not set but the tools_upload_flavor is set, then Packer will try to
-	// load the VMware tools from the VMware installation directory.
+	// The local path on your machine to the VMware Tools ISO file.
+	//
+	// ~> **Note:** If not set, but the `tools_upload_flavor` is set, the plugin
+	// will load the VMware Tools from the product installation directory.
 	ToolsSourcePath string `mapstructure:"tools_source_path" required:"false"`
 }
 
 func (c *ToolsConfig) Prepare(ctx *interpolate.Context) []error {
-	errs := []error{}
-	if c.ToolsUploadPath == "" {
-		if c.ToolsSourcePath != "" && c.ToolsUploadFlavor == "" {
-			errs = append(errs, fmt.Errorf("provide either "+
-				"'tools_upload_flavor' or 'tools_upload_path' with "+
-				"'tools_source_path'"))
-		}
+	if c.ToolsUploadPath != "" {
+		return nil
+	}
+
+	var errs []error
+
+	if c.ToolsSourcePath != "" && c.ToolsUploadFlavor == "" {
+		errs = append(errs, fmt.Errorf("provide either 'tools_upload_flavor' or 'tools_upload_path' with 'tools_source_path'"))
+	} else if c.ToolsUploadFlavor != "" && !slices.Contains(allowedToolsFlavorValues, c.ToolsUploadFlavor) {
+		errs = append(errs, fmt.Errorf("invalid 'tools_upload_flavor' specified: %s; must be one of %s", c.ToolsUploadFlavor, strings.Join(allowedToolsFlavorValues, ", ")))
+	} else {
+		c.ToolsUploadPath = fmt.Sprintf("%s.iso", c.ToolsUploadFlavor)
+	}
+
+	if c.ToolsSourcePath == "" {
 		c.ToolsUploadPath = "{{ .Flavor }}.iso"
 	}
 
