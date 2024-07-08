54ccaa3d690dcdad3bc6a6a4e70eba0b997b63e3
Ryan Johnson <ryan@tenthirtyam.org>
2024-07-07 22:12:52 -0400

    chore: deprecated `ssh_skip_request_pty`
    Deprecated `ssh_skip_request_pty` per comment:
    
    ```
    // These are deprecated, but we keep them around for BC
    // TODO(@mitchellh): remove"
    ```
    
    Signed-off-by: Ryan Johnson <ryan@tenthirtyam.org>

--- builder/vmware/common/ssh_config.go
+++ builder/vmware/common/ssh_config.go
@@ -6,6 +6,8 @@
 package common
 
 import (
+	"log"
+
 	"github.com/hashicorp/packer-plugin-sdk/communicator"
 	"github.com/hashicorp/packer-plugin-sdk/template/interpolate"
 )
@@ -13,14 +15,14 @@ import (
 type SSHConfig struct {
 	Comm communicator.Config `mapstructure:",squash"`
 
-	// These are deprecated, but we keep them around for BC
-	// TODO(@mitchellh): remove
+	// TODO: Deprecated. Remove in next major release
 	SSHSkipRequestPty bool `mapstructure:"ssh_skip_request_pty"`
 }
 
 func (c *SSHConfig) Prepare(ctx *interpolate.Context) []error {
 	if c.SSHSkipRequestPty {
 		c.Comm.SSHPty = false
+		log.Printf("[WARN] 'ssh_skip_request_pty' is deprecated and will be removed in the next major release.")
 	}
 
 	return c.Comm.Prepare(ctx)
